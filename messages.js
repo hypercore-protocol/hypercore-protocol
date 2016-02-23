f131a9219fda6e751991531776c1b3c02e955051
Mathias Buus <mathiasbuus@gmail.com>
2016-02-22 20:56:21 -0800

    fix standard

--- messages.js
+++ messages.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
 var protobuf = require('protocol-buffers')
+var path = require('path')
 
-module.exports = protobuf(fs.readFileSync(__dirname + '/schema.proto', 'utf-8'))
+module.exports = protobuf(fs.readFileSync(path.join(__dirname, '/schema.proto'), 'utf-8'))
