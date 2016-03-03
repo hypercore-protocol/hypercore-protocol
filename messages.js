c93763e278cf513b1a655710af575487d2853759
Mathias Buus <mathiasbuus@gmail.com>
2016-03-03 10:53:21 -0800

    move schema into file

--- messages.js
+++ messages.js
@@ -1,38 +1,5 @@
 var protobuf = require('protocol-buffers')
+var fs = require('fs')
+var path = require('path')
 
-module.exports = protobuf(`
-  message Open {
-    required bytes nonce = 1;
-    required bytes id = 2;
-  }
-
-  message Handshake {
-    optional bytes peer = 1;
-    repeated string extensions = 2;
-  }
-
-  message Request {
-    required uint64 block = 1;
-  }
-
-  message Response {
-    message Node {
-      required uint64 index = 1;
-      required uint64 size = 2;
-      required bytes hash = 3;
-    }
-
-    required uint64 block = 1;
-    required bytes data = 2;
-    repeated Node proof = 3;
-  }
-
-  message Have {
-    repeated uint64 blocks = 1;
-    optional bytes bitfield = 2;
-  }
-
-  message Cancel {
-    required uint64 block = 1;
-  }
-`)
+module.exports = protobuf(fs.readFileSync(path.join(__dirname, 'schema.proto')))
