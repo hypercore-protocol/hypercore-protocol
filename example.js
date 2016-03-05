bd3e2a443c45bc0c209a0e869ef6badd51902b86
Mathias Buus <mathiasbuus@gmail.com>
2016-03-05 15:32:33 -0800

    cleanup example

--- example.js
+++ example.js
@@ -1,57 +1,42 @@
 var protocol = require('./').use('ping')
 
 var p1 = protocol({
+  id: Buffer('mathias')
+})
+
+var p2 = protocol({
   id: Buffer('max'),
   join: function (id, cb) {
     cb(null, Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
   }
 })
 
-var p2 = protocol({
-  id: Buffer('mathias'),
-  join: function (id, cb) {
-    cb(null, Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
-  }
+p1.on('handshake', function () {
+  console.log('local: got handshake, remote peer:', p1.remoteId)
 })
 
-p1.on('handshake', function () {
-  console.log('got handshake, remote peer:', p1.remoteId)
+p2.on('handshake', function () {
+  console.log('remote: got handshake, local peer:', p2.remoteId)
 })
 
 p1.on('channel', function (channel) {
   channel.on('ping', function () {
-    console.log('got ping')
+    console.log('local: got ping, closing channel ...')
     channel.close()
   })
 
   channel.on('close', function () {
-    console.log('closing shop')
-  })
-
-  channel.on('request', function (message) {
-    console.log('remote requests:', message.block)
-    channel.response({block: message.block, data: Buffer('hello')})
+    console.log('local: channel closed')
   })
 })
 
 p2.on('channel', function (channel) {
   channel.on('close', function () {
-    console.log('closing')
+    console.log('remote: channel closed')
   })
 
-  channel.request({block: 1})
-  channel.request({block: 2})
   channel.ping()
-
-  channel.on('response', function (message) {
-    console.log('got response:', message)
-  })
 })
 
 p1.pipe(p2).pipe(p1)
-
-var ch = p1.join(Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
-
-ch.on('open', function () {
-  console.log('remote opened...')
-})
+p1.join(Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
