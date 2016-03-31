c9551864f0cc1b5a74cb8059227e1c5f61aba460
Mathias Buus <mathiasbuus@gmail.com>
2016-03-31 03:18:02 +0200

    fix example

--- example.js
+++ example.js
@@ -1,14 +1,19 @@
 var protocol = require('./').use('ping')
 
-var p1 = protocol({
-  id: Buffer('mathias')
-})
+var p1 = protocol({id: Buffer('mathias')})
 
-var p2 = protocol({
-  id: Buffer('max'),
-  join: function (id, cb) {
-    cb(null, Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
-  }
+var p2 = protocol({id: Buffer('max')}, function (publicId) {
+  var channel = p2.channel(Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
+
+  channel.on('close', function () {
+    console.log('remote: channel closed')
+  })
+
+  channel.on('handshake', function () {
+    channel.ping()
+  })
+
+  channel.handshake()
 })
 
 p1.on('handshake', function () {
@@ -19,24 +24,17 @@ p2.on('handshake', function () {
   console.log('remote: got handshake, local peer:', p2.remoteId)
 })
 
-p1.on('channel', function (channel) {
-  channel.on('ping', function () {
-    console.log('local: got ping, closing channel ...')
-    channel.close()
-  })
-
-  channel.on('close', function () {
-    console.log('local: channel closed')
-  })
-})
-
-p2.on('channel', function (channel) {
-  channel.on('close', function () {
-    console.log('remote: channel closed')
-  })
-
-  channel.ping()
-})
-
 p1.pipe(p2).pipe(p1)
-p1.join(Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
+
+var channel = p1.channel(Buffer('deadbeefdeadbeefdeadbeefdeadbeef'))
+
+channel.on('ping', function () {
+  console.log('local: got ping, closing channel ...')
+  channel.close()
+})
+
+channel.on('close', function () {
+  console.log('local: channel closed')
+})
+
+channel.handshake()
