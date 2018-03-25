c7127f34914841fffbaab772db4e79dba50238a3
Mathias Buus <mathiasbuus@gmail.com>
2018-03-26 00:10:13 +0200

    revert onclose triggered in .close(). should only run for remote close

--- feed.js
+++ feed.js
@@ -93,8 +93,6 @@ Feed.prototype.destroy = function (err) {
 }
 
 Feed.prototype.close = function () {
-  if (this.closed) return
-
   var i = this.stream.feeds.indexOf(this)
 
   if (i > -1) {
@@ -107,7 +105,6 @@ Feed.prototype.close = function () {
     if (this.stream.expectedFeeds <= 0 || --this.stream.expectedFeeds) return
 
     this.stream._prefinalize()
-    this._onclose()
   }
 }
 
